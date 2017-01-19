ff611a5ac8ad70aae8a97ad8770242fb0a4629cb
Aaron Trent <novacrazy@gmail.com>
2017-01-19 08:30:00 -0600

    Bump version

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@
 //! ```toml
 //! [dependencies]
 //! futures = "0.1"
-//! parallel-event-emitter = "0.2.1"
+//! parallel-event-emitter = "0.2.2"
 //! ```
 //!
 //! Example using a `String` as the key:
@@ -79,7 +79,7 @@
 //!
 //! ```toml
 //! [dependencies.parallel-event-emitter]
-//! version = "0.2.1"
+//! version = "0.2.2"
 //! features = ["default", "conservative_impl_trait"] # And maybe integer_atomics
 //! ```
 //!
@@ -92,7 +92,7 @@
 //!
 //! ```toml
 //! [dependencies.parallel-event-emitter]
-//! version = "0.2.1"
+//! version = "0.2.2"
 //! features = ["default", "integer_atomics"] # And maybe conservative_impl_trait
 //! ```
 //!
